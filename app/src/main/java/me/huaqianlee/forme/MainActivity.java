45c7415c0e051c75508b17f4f52d702a8609d2e1
huaqianlee <huaqianlee@gmail.com>
2017-08-16 18:28:28 +0800

    added navigationview

--- app/src/main/java/me/huaqianlee/forme/MainActivity.java
+++ app/src/main/java/me/huaqianlee/forme/MainActivity.java
@@ -1,12 +1,21 @@
 package me.huaqianlee.forme;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import me.huaqianlee.forme.util.LogUtil;
 
 public class MainActivity extends BaseActivity {
 
     private final static String TAG = "ForMe.MainActivity";
+    private DrawerLayout mlayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +24,44 @@ public class MainActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
+        mlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();//获得ToolBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
 
-        LogUtil.i(TAG,getString(R.string.test_log));
+/*        NavigationView  navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });*/
+
+
+        LogUtil.i(TAG, getString(R.string.test_log));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                break;
+            case android.R.id.home:
+                mlayout.openDrawer(GravityCompat.START);
+                break;
+
+            default:
+                break;
+
+        }
+        return true;
+    }
 }
