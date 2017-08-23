cc243fe86d8505e7d276bf6495eb25228d8fe68a
huaqianlee <huaqianlee@gmail.com>
2017-08-23 09:52:09 +0800

    added todo empty layout

--- app/src/main/java/me/huaqianlee/forme/MainActivity.java
+++ app/src/main/java/me/huaqianlee/forme/MainActivity.java
@@ -1,5 +1,7 @@
 package me.huaqianlee.forme;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -7,10 +9,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
+import android.view.*;
 import android.widget.Toast;
 import me.huaqianlee.forme.adapter.NavFuncAdapter;
 import me.huaqianlee.forme.bean.Func;
@@ -34,39 +33,37 @@ public class MainActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            toolbar.setContentInsetsRelative(50, 10);
+            //getWindow().setStatusBarColor(Color.TRANSPARENT);
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
         mlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBar actionBar = getSupportActionBar();//获得ToolBar
+ /*       ActionBar actionBar = getSupportActionBar();//获得ToolBar
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        }
+        }*/
 
-/*        NavigationView  navView = (NavigationView) findViewById(R.id.nav_view);
-        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                return false;
-            }
-        });*/
-
-        initFunc();
+        initFunc();  // 初始化侧边导航栏
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.nav_recycler_view);
-        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
         final NavFuncAdapter adapter = new NavFuncAdapter(funcList);
         recyclerView.setAdapter(adapter);
         adapter.setOnItemClickListener(new NavFuncAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                SelectedNavItem.setSlectedNavItem(position);
-                new MainViewSwitch().switchMainView(MainActivity.this);
+                SelectedNavItem.setSlectedNavItem(position); // 更新选中的位置
+                new MainViewSwitch().switchMainView(MainActivity.this); //切换主视图
+                switchTitle(position);
                 adapter.notifyDataSetChanged();
-                //Toast.makeText(MainActivity.this, "your click is litsen  on " + position, Toast.LENGTH_SHORT).show();
+                LogUtil.d(TAG, "You clicked the nav button!");
             }
         });
-
-
-        new MainViewSwitch().switchMainView(this);
 
         LogUtil.i(TAG, getString(R.string.test_log));
     }
@@ -101,20 +98,39 @@ public class MainActivity extends BaseActivity {
         return true;
     }
 
-    private Func [] funcs = {new Func ("Call",R.drawable.nav_call),new Func("Friends",R.drawable.nav_friends),new Func("Location",R.drawable.nav_location),
-    new Func("Mail", R.drawable.nav_mail), new Func("Tasks",R.drawable.nav_task)};
+    private void switchTitle(int position) {
+        ActionBar toolbar = getSupportActionBar();
+        switch (position) {
+            case SelectedNavItem.TODO:
+                toolbar.setTitle(R.string.todo_item);
+                break;
+            case SelectedNavItem.DATEEVENT:
+                toolbar.setTitle(R.string.date_event_item);
+                break;
+            default:
+                toolbar.setTitle(R.string.app_name);
+
+        }
+
+    }
+
+    private Func[] funcs = {new Func("Call", R.drawable.nav_call), new Func("Friends", R.drawable.nav_friends), new Func("Location", R.drawable.nav_location),
+            new Func("Mail", R.drawable.nav_mail), new Func("Tasks", R.drawable.nav_task)};
+
     private void initFunc() {
         funcList.clear();
         Func toDoFunc = new Func();
         toDoFunc.setImageId(R.drawable.todo_icon);
         toDoFunc.setName("ToDO");
         funcList.add(toDoFunc);
+        SelectedNavItem.setSlectedNavItem(SelectedNavItem.TODO);
+        new MainViewSwitch().switchMainView(this);
+        switchTitle(SelectedNavItem.TODO);
 
         Func toDoFunc2 = new Func("Lee", R.drawable.lee);
         funcList.add(toDoFunc2);
-        SelectedNavItem.setSlectedNavItem(SelectedNavItem.DATEEVENT);
 
-        for (int i = 0; i<funcs.length;i++) {
+        for (int i = 0; i < funcs.length; i++) {
             funcList.add(funcs[i]);
         }
     }
