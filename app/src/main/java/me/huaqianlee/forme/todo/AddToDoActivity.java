d340d4a14db3261f4f445af6c3cca99812335f79
huaqianlee <huaqianlee@gmail.com>
2017-09-27 17:51:51 +0800

    added func for AddToDoActivity

--- app/src/main/java/me/huaqianlee/forme/todo/AddToDoActivity.java
+++ app/src/main/java/me/huaqianlee/forme/todo/AddToDoActivity.java
@@ -9,24 +9,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.*;
+import me.huaqianlee.forme.MainActivity;
 import me.huaqianlee.forme.R;
 import me.huaqianlee.forme.base.BaseActivity;
 import me.huaqianlee.forme.util.LogUtil;
 
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import static me.huaqianlee.forme.todo.ToDoFragment.DARKTHEME;
+import static me.huaqianlee.forme.todo.ToDoFragment.TODOITEM;
 
-public class AddToDoActivity extends BaseActivity {
+public class AddToDoActivity extends BaseActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
     private String theme;
     private TodoItem item;
     private String itemText;
@@ -36,9 +42,12 @@ public class AddToDoActivity extends BaseActivity {
     private LinearLayout reminderDateLayout;
     private LinearLayout dateInputLayout;
     private EditText todoEdit;
+    private EditText timeEdit;
+    private EditText dateEdit;
     private SwitchCompat reminderSwitch;
     private FloatingActionButton todoFAB;
     private TextView newTodoText;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -66,7 +75,7 @@ public class AddToDoActivity extends BaseActivity {
             actionBar.setTitle("");
         }
         Intent intent = getIntent();
-        item = (TodoItem) intent.getSerializableExtra(ToDoFragment.TODOITEM);
+        item = (TodoItem) intent.getSerializableExtra(TODOITEM);
         itemText = item.getmToDoContent();
         hasReminder = item.hasReminder();
         reminderDate = item.getmToDoDate();
@@ -87,13 +96,16 @@ public class AddToDoActivity extends BaseActivity {
         newTodoText = (TextView)findViewById(R.id.new_todo_text);
         //setDateInputLayoutVisibleWithAnimations(false);
 
-        reminderDateLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hideKeyboard(todoEdit);
-                todoEdit.setFocusable(false);
-            }
-        });
+        timeEdit = (EditText) findViewById(R.id.add_todo_time_edit);
+        dateEdit = (EditText) findViewById(R.id.add_todo_date_edit);
+
+        /*Title*/
+        todoEdit.requestFocus();
+        todoEdit.setText(itemText);
+        InputMethodManager inputMethodManager = (InputMethodManager) this.getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        todoEdit.setSelection(itemText.length());
+
         todoEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -102,6 +114,74 @@ public class AddToDoActivity extends BaseActivity {
                 todoEdit.requestFocus();
             }
         });
+
+        todoEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+             itemText = s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        /*Finish FAB button*/
+        todoFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (todoEdit.length() <= 0) {
+                    todoEdit.setError(getString(R.string.todo_error));
+                } else if(reminderDate!=null && reminderDate.before(new Date())){
+                    makeResult(RESULT_CANCELED);
+                }
+                else{
+                    makeResult(RESULT_OK);
+                    finish();
+                }
+                hideKeyboard(todoEdit);
+            }
+        });
+
+
+        /*Reminder switch*/
+        //reminderSwitch.setChecked(hasReminder&&(reminderDate!=null));
+        if (hasReminder && (reminderDate != null)) {
+            //setReminderText();
+            reminderSwitch.setChecked(true);
+            setDateInputLayoutVisibleWithAnimations(true);
+        }
+        reminderSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    reminderDate = null;
+                }
+                hasReminder = isChecked;
+                setDateAndTime();
+                setDateInputLayoutVisibleWithAnimations(isChecked);
+                hideKeyboard(todoEdit);
+            }
+        });
+        if (reminderDate == null) {
+            reminderSwitch.setChecked(false);
+            setDateInputLayoutVisibleWithAnimations(false);
+        }
+
+        reminderDateLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard(todoEdit);
+                todoEdit.setFocusable(false);
+            }
+        });
+
         reminderSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -113,15 +193,135 @@ public class AddToDoActivity extends BaseActivity {
             }
         });
 
-        if (hasReminder && (reminderDate != null)) {
-            //setReminderText();
-            reminderSwitch.setChecked(true);
-            setDateInputLayoutVisibleWithAnimations(true);
+        /*DateInputLayout*/
+        setDateInputLayoutVisible(reminderSwitch.isChecked());
+        dateEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date ;
+                hideKeyboard(todoEdit);
+                if (item.getmToDoDate()!=null) {
+                    date = reminderDate;
+                } else {
+                    date = new Date();
+                }
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(AddToDoActivity.this, year, month, day);
+                if(theme.equals(DARKTHEME)){
+                    datePickerDialog.setThemeDark(true);
+                }
+                datePickerDialog.show(getFragmentManager(), "DateFragment");
+            }
+        });
+        timeEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date;
+                hideKeyboard(todoEdit);
+                if(item.getmToDoDate()!=null){
+                    date = reminderDate;
+                }
+                else{
+                    date = new Date();
+                }
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(AddToDoActivity.this, hour, minute, DateFormat.is24HourFormat(AddToDoActivity.this));
+                if(theme.equals(DARKTHEME)){
+                    timePickerDialog.setThemeDark(true);
+                }
+                timePickerDialog.show(getFragmentManager(), "TimeFragment");
+            }
+        });
+        setDateAndTime();
+    }
+
+    private void makeResult(int result) {
+        Intent i = new Intent();
+        if(itemText.length()>0){
+
+            String capitalizedString = Character.toUpperCase(itemText.charAt(0))+itemText.substring(1);
+            item.setmToDoContent(capitalizedString);
+        }
+        else{
+            item.setmToDoContent(itemText);
         }
 
-
-
+        if(reminderDate!=null){
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(reminderDate);
+            calendar.set(Calendar.SECOND, 0);
+            reminderDate = calendar.getTime();
+        }
+        item.setmHasReminder(hasReminder);
+        item.setmToDoDate(reminderDate);
+        item.setmToDoColor(itemColor);
+        i.putExtra(TODOITEM, item);
+        setResult(result, i);
     }
+
+    private void setDateAndTime() {
+        if (item.hasReminder() && reminderDate != null) {
+            String date = formatDate("d MMM, yyyy",reminderDate);
+            String formateToUse;
+            if (DateFormat.is24HourFormat(this)) {
+                formateToUse = "h:mm";
+            } else {
+                formateToUse = "h:mm a";
+            }
+            String time = formatDate(formateToUse, reminderDate);
+            timeEdit.setText(time);
+            dateEdit.setText(date);
+
+        } else {
+            dateEdit.setText(getString(R.string.date_reminder_default));
+            boolean isHour24 = DateFormat.is24HourFormat(this);
+            Calendar calendar = Calendar.getInstance();
+            if (isHour24) {
+                calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY)+1);
+            } else {
+                calendar.set(Calendar.HOUR, calendar.get(Calendar.HOUR)+1);
+            }
+            calendar.set(Calendar.MINUTE, 0);
+            reminderDate = calendar.getTime();
+            String timeText;
+            if (isHour24) {
+                timeText = formatDate("h:mm", reminderDate);
+            } else {
+                timeText = formatDate("h:mm a", reminderDate);
+            }
+            timeEdit.setText(timeText);
+        }
+    }
+
+   /*     mTimeEditText.setText(userTime);
+
+        String timeString;
+        if(time24){
+            timeString = formatDate("k:mm", mUserReminderDate);
+        }
+        else{
+            timeString = formatDate("h:mm a", mUserReminderDate);
+        }
+        mTimeEditText.setText(timeString);*/
+    public void setDateInputLayoutVisible(boolean checked){
+        if(checked){
+            dateInputLayout.setVisibility(View.VISIBLE);
+        }
+        else{
+            dateInputLayout.setVisibility(View.INVISIBLE);
+        }
+    }
+
 
     private void setDateInputLayoutVisibleWithAnimations(boolean b) {
         if(b){
@@ -212,5 +412,78 @@ public class AddToDoActivity extends BaseActivity {
     public static String formatDate(String format, Date date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat(format);
         return dateFormat.format(date);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hour, int minute) {
+        setTime(hour, minute);
+    }
+
+    private void setTime(int hour, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        if(reminderDate!=null){
+            calendar.setTime(reminderDate);
+        }
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        Log.d("OskarSchindler", "Time set: "+hour);
+        calendar.set(year, month, day, hour, minute, 0);
+        reminderDate = calendar.getTime();
+
+        setReminderText();
+        setTimeEditText();
+    }
+    public void  setTimeEditText(){
+        String dateFormat;
+        if(DateFormat.is24HourFormat(this)){
+            dateFormat = "k:mm";
+        }
+        else{
+            dateFormat = "h:mm a";
+
+        }
+        timeEdit.setText(formatDate(dateFormat, reminderDate));
+    }
+    public void  setDateEditText(){
+        String dateFormat = "d MMM, yyyy";
+        dateEdit.setText(formatDate(dateFormat, reminderDate));
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int month, int day) {
+        setDate(year, month, day);
+    }
+
+    private void setDate(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        int hour, minute;
+
+        Calendar reminderCalendar = Calendar.getInstance();
+        reminderCalendar.set(year, month, day);
+
+        if(reminderCalendar.before(calendar)){
+            Toast.makeText(this, "My time-machine is a bit rusty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(reminderDate!=null){
+            calendar.setTime(reminderDate);
+        }
+
+        if(DateFormat.is24HourFormat(this)){
+            hour = calendar.get(Calendar.HOUR_OF_DAY);
+        }
+        else{
+
+            hour = calendar.get(Calendar.HOUR);
+        }
+        minute = calendar.get(Calendar.MINUTE);
+
+        calendar.set(year, month, day, hour, minute);
+        reminderDate = calendar.getTime();
+        setReminderText();
+        setDateEditText();
     }
 }
